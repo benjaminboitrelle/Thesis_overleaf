c91faba54adcf7195d813ffd8550a32d2df0c4ef
Benjamin BOITRELLE <benjamin.boitrelle@gmail.com>
2016-04-06 15:16:48 +0200

    Modification of compileLatex to use latexmk and writing lepton collider advantages in ILC chapter

--- compileLatex.sh
+++ compileLatex.sh
@@ -1,39 +1,54 @@
-!#/bin/bash
+#!/bin/bash
 
 NAME=thesis_main
+OUTPUTDIR=build
 
-if [ ! -d "build" ]; then
-  mkdir build
+if [ ! -d $OUTPUTDIR ]; then
+  mkdir $OUTPUTDIR
 fi
 
-pdflatex -output-directory build/ $NAME.tex
-if [ $? -eq 0 ]; then 
-  echo "Producing pdf"
+if [[ -z $1 ]];then
+  echo "Generating the PDF file..."
+  latexmk -pdf -output-directory=$OUTPUTDIR $NAME.tex
+  if [ $? -eq 0 ]; then 
+    echo "...PDF file generated..."
+  else
+    echo "FAIL" 
+    exit $?; fi
+
+  if [ -f "build/$NAME.pdf" ]; then
+    echo "...moving it to the project directory"
+    mv $OUTPUTDIR/$NAME.pdf .
+  fi
+
 else
-  echo "FAIL" 
-  exit $?; fi
-  
-cp Bibliography/*.bib build
+  if [ "$1" == "clean" ]; then
+    echo "Cleaning build directory"
+    rm -rf $OUTPUTDIR/*
+  else
+    echo "Wrong input argument supplied"
+    exit 1
+  fi 
+fi
 
-cd build 
-
-bibtex $NAME
-echo "Linking bibliography"
-
-cd .. 
-
-pdflatex -output-directory build/ $NAME.tex
-if [ $? -eq 0 ]; then 
-  echo "Producing pdf"
-else
-  echo "FAIL" 
-  exit $?; fi
-
-pdflatex -output-directory build/ $NAME.tex
-if [ $? -eq 0 ]; then 
-  echo "Producing pdf"
-else
-  echo "FAIL" 
-  exit $?; fi
-
-mv build/$NAME.pdf .
+#cp Bibliography/*.bib build
+#
+#cd build 
+#
+#bibtex $NAME
+#
+#cd .. 
+#
+#pdflatex -output-directory build/ $NAME.tex
+#if [ $? -eq 0 ]; then 
+#  echo "Ready to compikle"
+#else
+#  echo "FAIL" 
+#  exit $?; fi
+#
+#pdflatex -output-directory build/ $NAME.tex
+#if [ $? -eq 0 ]; then 
+#  echo "Ready to compikle"
+#else
+#  echo "FAIL" 
+#  exit $?; fi
